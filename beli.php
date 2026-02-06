c08127c7e857d2f981ed4d7086b28743a5426fb4
modul10 <alvithops@gmail.com>
2026-02-06 20:10:40 +0700

    Update 2

--- beli.php
+++ beli.php
@@ -1,2 +1,4 @@
 //jika tidak ada eror data siap disimpan
-if (empty($namacustErr) && empty($emailErr))
+if (empty($namacustErr) && empty($emailErr) && empty($notelpErr) && empty($barangPilihErr)) {
+    
+}
