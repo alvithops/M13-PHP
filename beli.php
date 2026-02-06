1bb117792d202d8a728c6c7b0e47744b42f99818
modul10 <alvithops@gmail.com>
2026-02-06 20:49:18 +0700

    Update 6

--- beli.php
+++ beli.php
@@ -1,4 +1,7 @@
 //jika tidak ada eror data siap disimpan
 if (empty($namacustErr) && empty($emailErr) && empty($notelpErr) && empty($barangPilihErr)) {
+    $qty = 1;
+    $simpan = true;
 
+    require_once "koneksitoko.php";
 }
