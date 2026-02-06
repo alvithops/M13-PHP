922fbbaf15e8f34d131ad50640b785a1ceb9d4c1
modul10 <alvithops@gmail.com>
2026-02-06 22:09:15 +0700

    Update 96

--- beli.php
+++ beli.php
@@ -79,6 +79,6 @@ if($simpan){
     echo "Pembelian gagal<br>";
 }
 setcookie('keranjang',$barangPilih,time()-3600);
-header("Location:buktibeli.php")
+header("Location:buktibeli.php?idhjual")
 //menutup koneksi
 mysqli_close($kon);
