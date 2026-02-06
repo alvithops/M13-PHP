b62dfd474e6046cf041308a104958e15b7d0e019
modul10 <alvithops@gmail.com>
2026-02-06 20:11:38 +0700

    Update 3

--- beli.php
+++ beli.php
@@ -1,4 +1,5 @@
 //jika tidak ada eror data siap disimpan
 if (empty($namacustErr) && empty($emailErr) && empty($notelpErr) && empty($barangPilihErr)) {
+    echo "<h3>Data Siap Disimpan</h3>";
     
 }
