948667ebb3d6bd71413196a386445f90b4549d5b
modul10 <alvithops@gmail.com>
2026-02-06 23:20:27 +0700

    Update 189

--- buktibeli.php
+++ buktibeli.php
@@ -44,7 +44,7 @@
         echo "NO. NOTA : ".date("Ymd").$row['tanggal']."<br>";
         echo "TANGGAL : ".$row['tanggal']."<br>";
         echo "NAMA :".$row['namacust']."<br>";
-        $sql = "select barang.nama, djual.harga, djual.qty,(djual.harga"
+        $sql = "select barang.nama, djual.harga, djual.qty,(djual.harga * djual"
     </body>
 
 </html>
