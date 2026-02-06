547bbcccee138a4fa76f74a9acf14594b78b3b26
modul10 <alvithops@gmail.com>
2026-02-06 23:08:13 +0700

    Update 166

--- buktibeli.php
+++ buktibeli.php
@@ -41,7 +41,7 @@
         $row = mysqli_fetch_array($hasil);
         echo "<pre>";
         echo "<h2>Bukti Pembelian</h2>";
-        echo "NO. NOTA : ".date"
+        echo "NO. NOTA : ".date("Ymd")"
     </body>
 
 </html>
