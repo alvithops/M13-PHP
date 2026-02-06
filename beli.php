2bd28956dab3fcc3d1d67db7f9ed1cc82ec430a7
modul10 <alvithops@gmail.com>
2026-02-06 20:14:19 +0700

    Update 5

--- beli.php
+++ beli.php
@@ -1,5 +1,4 @@
 //jika tidak ada eror data siap disimpan
 if (empty($namacustErr) && empty($emailErr) && empty($notelpErr) && empty($barangPilihErr)) {
-    echo "<h3>Data Siap Disimpan</h3>";
-    setcookie('keranjang',$barangPilih,time()+3600);
+
 }
